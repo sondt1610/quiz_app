642b26153799e505ecd4a090b7caba23cd413c31
duynq7169 <nguyenquocduy1001@gmail.com>
2020-09-03 13:30:37 +0700

    success

--- lib/models/quiz.dart
+++ lib/models/quiz.dart
@@ -1,8 +1,10 @@
 import 'package:quiz_app/models/question.dart';
 
 class Quiz {
+  String documentID;
   String imgUrl;
   String title;
   String desc;
   List<Question> questionList;
+  Quiz(this.imgUrl, this.title, this.desc, this.documentID);
 }
--- lib/screens/create_quiz.dart
+++ lib/screens/create_quiz.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:quiz_app/widgets/customButton.dart';
+import 'package:quiz_app/widgets/customTextField.dart';
 
 class CreateQuiz extends StatefulWidget {
   @override
@@ -6,8 +9,90 @@ class CreateQuiz extends StatefulWidget {
 }
 
 class _CreateQuizState extends State<CreateQuiz> {
+  String title, desc, imageURL;
+  bool showProgress = false;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Create Quiz"),
+      ),
+      body: Center(
+        child: ModalProgressHUD(
+          inAsyncCall: showProgress,
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Form(
+              autovalidate: true,
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Create",
+                    style:
+                        TextStyle(fontWeight: FontWeight.w800, fontSize: 20.0),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  CustomTextField(
+                    hintText: "Title",
+                    onSaved: (String value) {
+                      title = value;
+                    },
+                    validator: null,
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  CustomTextField(
+                      hintText: "Desc",
+                      obsecure: true,
+                      onSaved: (String value) {
+                        desc = value;
+                      },
+                      validator: null),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  CustomTextField(
+                      hintText: "imgURL",
+                      obsecure: true,
+                      onSaved: (String value) {
+                        desc = value;
+                      },
+                      validator: null),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  CustomButton(
+                      label: 'Next',
+                      onPressed: () async {
+                        if (!_formKey.currentState.validate()) {
+                          return;
+                        }
+                        _formKey.currentState.save();
+                        setState(() {
+                          showProgress = true;
+                        });
+                        try {} catch (e) {
+                          print('SignUp error 2: ' + e.toString());
+                        }
+                      }),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
